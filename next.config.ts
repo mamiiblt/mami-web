5be707acbd5d4b83623de194db1f4776fbcdfaf8
mamiiblt <macesgamesstudios@gmail.com>
2025-12-14 18:58:28 +0000

    feat: Update dependencies, switch to Turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-
-      fs: false,
-    };
-    return config;
-  },
+  turbopack: {},
   images: {
     remotePatterns: [
       {
