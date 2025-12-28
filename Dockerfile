d48cf59feaf8316925ead326ad5bd2dfc4c000c7
mamiiblt <macesgamesstudios@gmail.com>
2025-12-28 05:41:44 +0000

    feat: Use Dockerfile for building / starting app

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:22-alpine
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install
+COPY . .
+ENV NODE_ENV=production
+RUN npm run build
+EXPOSE 3000
+CMD ["npm", "run", "start"]
--- src/lib/serverDatabase.ts
+++ src/lib/serverDatabase.ts
@@ -1,19 +1,25 @@
 import { Pool } from "pg";
 
 const globalForPg = globalThis as unknown as {
-  pgPool?: Pool
+    pgPool?: Pool;
 };
 
 export const pgPool =
     globalForPg.pgPool ??
     new Pool({
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOSTNAME,
-      port: Number(process.env.DB_PORT),
-      database: process.env.DB_DATABASE
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        host:
+            process.env.NODE_ENV === "development"
+                ? process.env.DB_HOSTNAME_PUBLIC
+                : process.env.DB_HOSTNAME_INTERNAL,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_DATABASE,
+        max: 5,
+        idleTimeoutMillis: 30_000,
+        connectionTimeoutMillis: 2_000,
     });
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPg.pgPool = pgPool;
+    globalForPg.pgPool = pgPool;
 }
