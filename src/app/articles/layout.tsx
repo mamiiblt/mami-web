150e1d5208090453ee4c025a75869fa258f33a2d
mamiiblt <mami@mamii.dev>
2026-01-16 20:55:58 +0300

    feat: Add a static SEO for /articles

--- src/app/articles/layout.tsx
+++ src/app/articles/layout.tsx
@@ -1,10 +1,32 @@
 import { Metadata } from "next";
 import { defaultMetadata } from "@/config/metadata";
 
+const seoData = {
+  title: "mamii's articles",
+  description: "You can access all of mamii's articles through this page.",
+  bannerUrl: "/page_banners/articles.png"
+}
+
 export const metadata: Metadata = {
   ...defaultMetadata,
-  title: `Articles`,
-  description: "You can find all my articles here",
+  title: seoData.title,
+  description: seoData.description,
+  openGraph: {
+    title: seoData.title,
+    description: seoData.description,
+    locale: "en_US",
+    images: [
+      { url: seoData.bannerUrl, width: 1024, height: 558, alt: seoData.title, type: "image/png" },
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@mamiiblt",
+    title: seoData.title,
+    creator: "@mamiiblt",
+    description: seoData.description,
+    images: [seoData.bannerUrl]
+  }
 };
 
 export default function ArticlesLayout({
