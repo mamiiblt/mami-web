f2f40468a8f3753d9107a96bf660665433f99f48
mamiiblt <mamii@mamii.dev>
2026-06-13 22:13:32 +0300

    feat: Added `Deutsch`, `Español` and `Français` languages

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -47,34 +47,6 @@ export function formatDateWithTime(date: any, localeCode = "en-US"): string {
   })
 }
 
-export function formatISODate(date: Date) {
-  return date.toISOString().split("T")[0];
-}
-
-export function getLanguageDisplayName(languageCode: string, displayLocale = "en"): string {
-  if (!languageCode) return "";
-
-  const code = languageCode.trim().replace(/_/g, "-");
-  const splittedLangCode = code.split("-");
-  const base = splittedLangCode[0]
-  const region = splittedLangCode[1]
-
-  try {
-    const dn = new Intl.DisplayNames([displayLocale], { type: "language" });
-    var name = ""
-    if (base.toLowerCase() == region.toLowerCase() || base == "en") {
-      name = dn.of(base)
-    } else {
-      name = dn.of(code)
-    }
-    if (name) return capitalizeFirstLetter(name, displayLocale);
-  } catch {
-    const name = ISO6391.getName(base) || ISO6391.getNativeName(base);
-    if (name) return capitalizeFirstLetter(name, displayLocale);
-    return code;
-  }
-}
-
 export const generatePageNumbers = (totalPageSize: number, currentPage: number) => {
   const pages = [];
   const maxVisiblePages = 5;
